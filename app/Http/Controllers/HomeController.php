b911a0c9cedc15dc7e0c5c9fdbdadb9556cbfd6e
yusufkggoglu <yusufkggoglu@icloud.com>
2022-03-23 21:33:33 +0300

    Controller changed.

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index()
+    {
+        return view(view('home.index'));
+    }
+
+    public function test($id,$name)
+    {
+        //verileri dataya yükleyipde ulaşabiliriz.
+        $data['id']=$id;
+        $data['name']=$name;
+        return view('home.test',$data);
+        /*
+        echo "ID number : ", $id;
+        echo "<br> Name : ", $name;
+        for($i=1;$i<$id;$i++)
+        {
+            echo "<br> $i - $name";
+        }*/
+    }
+}
