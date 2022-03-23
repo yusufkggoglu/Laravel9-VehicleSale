7f87f2599f4f9016de2d6d96f36b85d5f331fc1b
yusufkggoglu <yusufkggoglu@icloud.com>
2022-03-23 21:33:17 +0300

    Routes Changed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home.index');
+})->name('home');
+//Route::get('/home', [HomeController::class, 'index']);
+
+Route::redirect('/anasayfa', '/home')->name('anasayfa');
+
+//parametre olarak sadece sayı göndermek,string gönderirsen hata verir.
+//Route::get('/test/{id}', [HomeController::class, 'test'])->where('id','[0-9]+');
+//Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->where(['id'=>'[0-9]+','name'=>'[A-Za-z]+']);
+//veya
+Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->whereNumber('id')->whereAlpha('name')->name('test');
+
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
