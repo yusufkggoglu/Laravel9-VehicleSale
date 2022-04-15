0677107eb4d6b8481162876597b94e138e1c8356
yusufkggoglu <yusufkggoglu@icloud.com>
2022-04-15 19:57:09 +0300

    Admin Sidebar Links & Icons

--- resources/views/admin/_header.blade.php
+++ resources/views/admin/_header.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
     <div class="navbar-brand-wrapper d-flex justify-content-center">
         <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">
-            <a class="navbar-brand brand-logo" href="index.html"><img
+            <a class="navbar-brand brand-logo" href="{{route('admin_home')}}"><img
                     src="{{asset('assets')}}/admin/images/logo.svg" alt="logo"/></a>
             <a class="navbar-brand brand-logo-mini" href="index.html"><img
                     src="{{asset('assets')}}/admin/images/logo-mini.svg" alt="logo"/></a>
@@ -25,52 +25,14 @@
                 </div>
             </li>
         </ul>
+        <!-- messages -->
         <ul class="navbar-nav navbar-nav-right">
             <li class="nav-item dropdown me-1">
                 <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center"
-                   id="messageDropdown" href="#" data-bs-toggle="dropdown">
+                   id="messageDropdown" href="admin/messages">
                     <i class="mdi mdi-message-text mx-0"></i>
-                    <span class="count"></span>
+                   <!-- bildirim = <span class="count"></span> -->
                 </a>
-                <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="messageDropdown">
-                    <p class="mb-0 font-weight-normal float-left dropdown-header">Messages</p>
-                    <a class="dropdown-item">
-                        <div class="item-thumbnail">
-                            <img src="{{asset('assets')}}/admin/images/faces/face4.jpg" alt="image" class="profile-pic">
-                        </div>
-                        <div class="item-content flex-grow">
-                            <h6 class="ellipsis font-weight-normal">David Grey
-                            </h6>
-                            <p class="font-weight-light small-text text-muted mb-0">
-                                The meeting is cancelled
-                            </p>
-                        </div>
-                    </a>
-                    <a class="dropdown-item">
-                        <div class="item-thumbnail">
-                            <img src="{{asset('assets')}}/admin/images/faces/face2.jpg" alt="image" class="profile-pic">
-                        </div>
-                        <div class="item-content flex-grow">
-                            <h6 class="ellipsis font-weight-normal">Tim Cook
-                            </h6>
-                            <p class="font-weight-light small-text text-muted mb-0">
-                                New product launch
-                            </p>
-                        </div>
-                    </a>
-                    <a class="dropdown-item">
-                        <div class="item-thumbnail">
-                            <img src="{{asset('assets')}}/admin/images/faces/face3.jpg" alt="image" class="profile-pic">
-                        </div>
-                        <div class="item-content flex-grow">
-                            <h6 class="ellipsis font-weight-normal"> Johnson
-                            </h6>
-                            <p class="font-weight-light small-text text-muted mb-0">
-                                Upcoming board meeting
-                            </p>
-                        </div>
-                    </a>
-                </div>
             </li>
             <li class="nav-item dropdown me-4">
                 <a class="nav-link count-indicator dropdown-toggle d-flex align-items-center justify-content-center notification-dropdown"
@@ -128,7 +90,7 @@
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
                     @auth()
-                        <a class="dropdown-item">
+                        <a href="admin/settings" class="dropdown-item">
                             <i class="mdi mdi-settings text-primary"></i>
                             Settings
                         </a>
--- resources/views/admin/_sidebar.blade.php
+++ resources/views/admin/_sidebar.blade.php
@@ -2,84 +2,46 @@
     <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <ul class="nav">
             <li class="nav-item">
-                <a class="nav-link" href="{{route('admin_home')}}">
-                <i class="mdi mdi-home menu-icon"></i>
-                <span class="menu-title">Home</span>
+                <a class="nav-link" href="/admin">
+                    <i style="color:darkblue" class="mdi mdi-home menu-icon"></i>
+                    <span class="menu-title">Home</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/car">
+                    <i style="color:darkblue" class="mdi mdi-car menu-icon"></i>
+                    <span class="menu-title">Cars</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{route('admin_category')}}">
-                    <i class="mdi mdi-view-headline menu-icon"></i>
-                    <span class="menu-title">Category</span>
+                <a class="nav-link" href="/admin/category">
+                    <i style="color:darkblue" class="mdi mdi-apps menu-icon"></i>
+                    <span class="menu-title">Categories</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="">
-                    <i class="mdi mdi-view-headline menu-icon"></i>
-                    <span class="menu-title">Products</span>
+                <a class="nav-link" href="/admin/users">
+                    <i style="color:darkblue" class="mdi mdi-account-multiple menu-icon"></i>
+                    <span class="menu-title">Users</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
-                   aria-controls="ui-basic">
-                    <i class="mdi mdi-circle-outline menu-icon"></i>
-                    <span class="menu-title">UI Elements</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="ui-basic">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"><a class="nav-link" href="{{asset('assets')}}/admin/pages/ui-features/buttons.html">Buttons</a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{asset('assets')}}/admin/pages/ui-features/typography.html">Typography</a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{asset('assets')}}/admin/pages/forms/basic_elements.html">
-                    <i class="mdi mdi-view-headline menu-icon"></i>
-                    <span class="menu-title">Form elements</span>
+                <a class="nav-link" href="/admin/comment">
+                    <i style="color:darkblue" class="mdi mdi-comment-text menu-icon"></i>
+                    <span class="menu-title">Comments</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{asset('assets')}}/admin/pages/charts/chartjs.html">
-                    <i class="mdi mdi-chart-pie menu-icon"></i>
-                    <span class="menu-title">Charts</span>
+                <a class="nav-link" href="/admin/faq">
+                    <i style="color:darkblue" class="mdi mdi-comment-question-outline menu-icon "></i>
+                    <span class="menu-title">FAQ</span>
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{asset('assets')}}/admin/pages/tables/basic-table.html">
-                    <i class="mdi mdi-grid-large menu-icon"></i>
-                    <span class="menu-title">Tables</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{asset('assets')}}/admin/admin/pages/icons/mdi.html">
-                    <i class="mdi mdi-emoticon menu-icon"></i>
-                    <span class="menu-title">Icons</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
-                    <i class="mdi mdi-account menu-icon"></i>
-                    <span class="menu-title">User Pages</span>
-                    <i class="menu-arrow"></i>
-                </a>
-                <div class="collapse" id="auth">
-                    <ul class="nav flex-column sub-menu">
-                        <li class="nav-item"><a class="nav-link" href="{{asset('assets')}}/admin/pages/samples/login.html"> Login </a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{asset('assets')}}/admin/pages/samples/login-2.html"> Login 2 </a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{asset('assets')}}/admin/pages/samples/register.html"> Register </a></li>
-                        <li class="nav-item"><a class="nav-link" href="{{asset('assets')}}/admin/pages/samples/register-2.html"> Register 2 </a>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="{{asset('assets')}}/admin/pages/samples/lock-screen.html"> Lockscreen </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="{{asset('assets')}}/admin/documentation/documentation.html">
-                    <i class="mdi mdi-file-document-box-outline menu-icon"></i>
-                    <span class="menu-title">Documentation</span>
+                <a class="nav-link" href="/admin/social">
+                    <i style="color:darkblue" class="mdi mdi-earth menu-icon"></i>
+                    <span class="menu-title">Social</span>
                 </a>
             </li>
         </ul>
