271fea83fa78fc3d627e1b98288861e261571110
yusufkggoglu <yusufkggoglu@icloud.com>
2022-04-19 06:06:08 +0300

    Route Group Prefix

--- routes/web.php
+++ routes/web.php
@@ -1,50 +1,41 @@
 <?php
 
+use App\Http\Controllers\AdminPanel\AdminCarController;
+use App\Http\Controllers\AdminPanel\CategoryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/home', function () {
     return view('welcome');
 });
 
-//Ana Sayfa
+//**************************ANA SAYFA ROUTES*******************************
 Route::get('/', function () {
     return view('home.index');
 })->name('home');
 Route::redirect('/anasayfa', '/')->name('anasayfa');
-//end
-
-//TEST
-//parametre olarak sadece sayı göndermek,string gönderirsen hata verir.
-//Route::get('/test/{id}', [HomeController::class, 'test'])->where('id','[0-9]+');
-//Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->where(['id'=>'[0-9]+','name'=>'[A-Za-z]+']);
-Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->whereNumber('id')->whereAlpha('name')->name('test');
-//end
-
 Route::get('/aboutus', [HomeController::class, 'aboutus'])->name('aboutus');
 
-//ADMİN ROUTES
-Route::get('/admin', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin_home')->middleware('auth');
-//LOGIN ROUTES
-Route::get('/admin/login', [HomeController::class, 'login'])->name('admin_login');
-Route::post('/admin/logincheck', [HomeController::class, 'logincheck'])->name('admin_logincheck');
-Route::get('/admin/logout', [HomeController::class, 'logout'])->name('admin_logout');
-// ADMİN CATEGOTY ROUTES
-Route::middleware('auth')->prefix('admin')->group(function () {
-    // Route::get('/', [\App\Http\Controllers\AdminPanel\CategoryController::class, 'index'])->name('admin_home');
-    Route::get('category', [\App\Http\Controllers\AdminPanel\CategoryController::class, 'index'])->name('admin_category');
-    Route::get('category/create', [\App\Http\Controllers\AdminPanel\CategoryController::class, 'create'])->name('admin_category_create');
-    Route::post('category/store', [\App\Http\Controllers\AdminPanel\CategoryController::class, 'store'])->name('admin_category_store');
-    Route::get('category/edit/{id}', [\App\Http\Controllers\AdminPanel\CategoryController::class, 'edit'])->name('admin_category_edit');
-    Route::post('category/update/{id}', [\App\Http\Controllers\AdminPanel\CategoryController::class, 'update'])->name('admin_category_update');
-    Route::get('category/delete', [\App\Http\Controllers\AdminPanel\CategoryController::class, 'delete'])->name('admin_category_delete');
-    Route::get('category/show', [\App\Http\Controllers\AdminPanel\CategoryController::class, 'show'])->name('admin_category_show');
+//**********************ADMİN PANEL ROUTES****************************
+Route::prefix('admin')->name('admin_')->group(function () {
+    Route::get('/', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('home')->middleware('auth');
+    //*************************LOGIN ROUTES****************************
+    Route::get('/login', [HomeController::class, 'login'])->name('login');
+    Route::post('/logincheck', [HomeController::class, 'logincheck'])->name('logincheck');
+    Route::get('/logout', [HomeController::class, 'logout'])->name('logout');
+    //****************ADMİN CATEGORY ROUTES*****************************
+    Route::prefix('category')->name('category_')->controller(CategoryController::class)->group(function () {
+        Route::get('/', 'index')->name('home');
+        Route::get('/create', 'create')->name('create');
+        Route::post('/store', 'store')->name('store');
+        Route::get('/edit/{id}', 'edit')->name('edit');
+        Route::post('/update/{id}', 'update')->name('update');
+        Route::get('/delete/{id}', 'destroy')->name('delete');
+        Route::get('/show/{id}', 'show')->name('show');
+    });
 
 });
 
-
-//end
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
